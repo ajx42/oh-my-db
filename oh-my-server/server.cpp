e7a886c7b2d19008e3a5510ae86793bddac8570b
hcoffey1 <hcoffey1@vols.utk.edu>
2023-03-24 19:23:34 -0500

    Clean up code

--- oh-my-server/server.cpp
+++ oh-my-server/server.cpp
@@ -17,12 +17,24 @@
 class OhMyDBService final : public ohmydb::OhMyDB::Service
 {
 private:
+    leveldb::DB *db;
+    leveldb::Options options;
+
 public:
     explicit OhMyDBService()
     {
+        // leveldb setup
+        options.create_if_missing = true;
+        leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
+        if (!status.ok())
+        {
+            std::cerr << "Unable to open/create database" << std::endl;
+        }
     }
 
     grpc::Status TestCall(grpc::ServerContext *, const ohmydb::Cmd *, ohmydb::Ack *);
+    grpc::Status Put(grpc::ServerContext *, const ohmydb::PutRequest *, ohmydb::Ack *);
+    grpc::Status Get(grpc::ServerContext *, const ohmydb::GetRequest *, ohmydb::GetResponse *);
 };
 
 grpc::Status OhMyDBService::TestCall(
@@ -33,20 +45,41 @@ grpc::Status OhMyDBService::TestCall(
     return grpc::Status::OK;
 }
 
-int main()
+grpc::Status OhMyDBService::Put(
+    grpc::ServerContext *, const ohmydb::PutRequest *request, ohmydb::Ack *ack)
 {
-    // leveldb setup
-    leveldb::DB *db;
-    leveldb::Options options;
-    options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
-    if (!status.ok())
+    leveldb::Status status = db->Put(leveldb::WriteOptions(), request->key(), request->value());
+    if (status.ok())
     {
-        std::cerr << "Unable to open/create database" << std::endl;
-        return 1;
+        ack->set_ok(0);
+    }
+    else
+    {
+        ack->set_ok(-1);
+    }
+    return grpc::Status::OK;
+}
+
+grpc::Status OhMyDBService::Get(
+    grpc::ServerContext *, const ohmydb::GetRequest *request, ohmydb::GetResponse *response)
+{
+    std::string value;
+    leveldb::Status status = db->Get(leveldb::ReadOptions(), request->key(), &value);
+    if (status.ok())
+    {
+        response->set_ok(0);
+        response->set_value(value);
+    }
+    else
+    {
+        response->set_ok(-1);
     }
 
-    // TODO: lets setup leveldb inside the service
+    return grpc::Status::OK;
+}
+
+int main()
+{
     std::string server_addr("0.0.0.0:50051");
     OhMyDBService service;
 
@@ -63,123 +96,4 @@ int main()
     server->Wait();
 
     return 0;
-
-#if 0
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    char buffer[1024] = {0};
-    std::string response;
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        return 1;
-    }
-
-    // Attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        return 1;
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        return 1;
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        return 1;
-    }
-    std::cout << "Server listening on port " << PORT << std::endl;
-
-    while (true)
-    {
-        // Accept a new client connection.
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
-        {
-            perror("accept");
-            return 1;
-        }
-
-        std::cout << "New client connected: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << std::endl;
-
-        // Process the client requests.
-        while (true)
-        {
-            std::memset(buffer, 0, sizeof(buffer));
-
-            // Receive the client's request.
-            ssize_t bytes_received = recv(new_socket, buffer, sizeof(buffer), 0);
-            if (bytes_received < 0)
-            {
-                perror("recv");
-                return 1;
-            }
-            else if (bytes_received == 0)
-            {
-                // The client has closed the connection.
-                std::cout << "Client disconnected: " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << std::endl;
-                break;
-            }
-
-            std::string request(buffer, bytes_received);
-            std::istringstream iss(request);
-            std::string command, key, value;
-            iss >> command >> key;
-            if (command == "put")
-            {
-                iss >> value;
-                leveldb::Status status = db->Put(leveldb::WriteOptions(), key, value);
-                if (status.ok())
-                {
-                    response = "OK";
-                }
-                else
-                {
-                    response = "ERROR";
-                }
-            }
-            else if (command == "get")
-            {
-                std::string value;
-                leveldb::Status status = db->Get(leveldb::ReadOptions(), key, &value);
-                if (status.ok())
-                {
-                    response = "OK " + value;
-                }
-                else
-                {
-                    response = "ERROR";
-                }
-            }
-            else if (command == "quit")
-            {
-                response = "GOODBYE";
-                break;
-            }
-            else
-            {
-                response = "ERROR";
-            }
-
-            response += "\n";
-            // Send the response back to the client. end with newline
-            send(new_socket, response.c_str(), response.length(), 0);
-        }
-
-        close(new_socket);
-    }
-
-    return 0;
-#endif
 }
