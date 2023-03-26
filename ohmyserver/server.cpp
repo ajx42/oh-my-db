c1d103ced7cb7af1db0706b972e3d6dbdad2e032
tauChang <tomy.1516@gmail.com>
2023-03-26 15:26:02 -0500

    Add csv parsing

--- ohmyserver/server.cpp
+++ ohmyserver/server.cpp
@@ -3,12 +3,37 @@
 #include <string>
 #include <sstream>
 #include <unistd.h>
+#include <argparse/argparse.hpp>
 #include "server.h"
 
 
-int main()
+int main(int argc, char **argv)
 {
-    std::string server_addr("0.0.0.0:50051");
+    argparse::ArgumentParser program("server");
+    program.add_argument("--config")
+        .required()
+        .help("Config file path");
+
+    program.add_argument("--port")
+        .required()
+        .help("Config file path");
+
+    try {
+        program.parse_args( argc, argv );
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    // parse arguments
+    auto port = program.get<std::string>("--port");
+    auto config_path = program.get<std::string>("--config");
+
+    auto servers = ParseConfig(config_path);
+
+    std::string server_addr("0.0.0.0:" + port);
     OhMyDBService service;
 
     grpc::EnableDefaultHealthCheckService(true);
--- ohmyserver/server.h
+++ ohmyserver/server.h
@@ -2,6 +2,16 @@
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
 #include "db.grpc.pb.h"
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+
+typedef struct ServerInfo {
+    std::string ip;
+    int port;
+    std::string name;
+} ServerInfo;
 
 class OhMyDBService final : public ohmydb::OhMyDB::Service
 {
@@ -65,4 +75,45 @@ grpc::Status OhMyDBService::Get(
     }
 
     return grpc::Status::OK;
+}
+
+// parse a csv file and return a vector of ServerInfo
+// the first line of the file is header: name, intf_ip, host_ip, hostname, port, username
+// but the header columns might be unordered, so we need to find the index of each column
+// only want name, intf_ip, port
+std::vector<ServerInfo> ParseConfig(std::string filename)
+{
+    std::vector<ServerInfo> servers;
+    std::ifstream file(filename);
+    std::string line;
+    // parse header
+    std::getline(file, line);
+    std::stringstream ss(line);
+    std::string token;
+    std::unordered_map<std::string, int> header;
+
+    while (std::getline(ss, token, ','))
+    {
+        header[token] = header.size();
+    }
+
+    // parse each node
+    while (std::getline(file, line))
+    {
+        // check if line is empty
+        if (line.empty()) break;
+        std::stringstream ss(line);
+        std::string token;
+        std::vector<std::string> tokens;
+        while (getline(ss, token, ','))
+        {
+            tokens.push_back(token);
+        }
+        ServerInfo server;
+        server.name = tokens[header["name"]];
+        server.ip = tokens[header["intf_ip"]];
+        server.port = stoi(tokens[header["port"]]);
+        servers.push_back(server);
+    }
+    return servers;
 }
