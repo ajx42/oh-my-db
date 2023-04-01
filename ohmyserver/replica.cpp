e1d42ec9515ddb48302d8745423fbc72b1d21752
hcoffey1 <hcoffey1@vols.utk.edu>
2023-03-31 19:40:38 -0500

    Initial election skeleton code

--- ohmyserver/replica.cpp
+++ ohmyserver/replica.cpp
@@ -14,6 +14,37 @@
 #include "RaftService.H"
 #include "OhMyReplica.H"
 
+//Leader election notes:
+//Server starts in follower state, maintains follower
+//state so long as it receives heartbeat from leader
+//If no heartbeat within election timeout phase,
+//then server begins election to choose new leader.
+
+//To begin election, follower increments current term
+//and switches to "canidate" state. Votes for itself
+//and requests votes from others.
+
+//Three conditions can break election,
+//* the server wins
+//* another server wins
+//* nobody wins within a given time period
+
+//When a candidate wins, it sends a heartbeat to all
+//other servers to establish its rule
+
+//If a candidate receives a heartbeat from a server
+//with a term >= the candidate's term, it accepts it as leader.
+//Otherwise, reject the heartbeat and continue the election.
+
+//To prevent repeat splits, election time outs are chosen
+//randomly (150-300ms in the paper). 
+
+//I think there needs to be a further grey period on this though
+//for restarting an election. Each node has a randomized
+//election timeout time, where if the election fails,
+//it then waits for a random amount of time before
+//trying again? Essentially two election timeouts
+
 int main(int argc, char **argv)
 {
   argparse::ArgumentParser program("server");
@@ -65,5 +96,6 @@ int main(int argc, char **argv)
   }
   // -- 
 
-  while(1) { std::this_thread::sleep_for(std::chrono::seconds(10)); }
+  //Busy loop, handles election timeouts
+  ReplicaManager::Instance().busy();
 }
