c2be628fdfa3cbee5e794e0faa24f98806bbac14
tauChang <tomy.1516@gmail.com>
2023-04-09 10:27:18 -0500

    switch case and comment

--- scripts/membership/run_admin.sh
+++ scripts/membership/run_admin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./build/ohmyserver/admin --id 4 --config ./config.csv --op rm
+# ./build/ohmyserver/admin --id 2 --config ./config.csv --op rm
 # Note: Node3 should be up and running before adding it to the cluster
 ./build/ohmyserver/admin --id 4 --config ./config.csv --op add --ip 10.10.1.1 --db_port 12349 --raft_port 8084 --name "Node3" 
 
